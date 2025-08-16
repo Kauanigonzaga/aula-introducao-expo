9209e57f46bd4bcaaf52e9ee5ac9499fa7aa7939
Kauani Nunes Costa Gonzaga de Melo <nuneskauani47@gmail.com>
2025-08-15 22:45:43 -0300

    Começando exemplo 01

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Exemplo001 from './src/exemplos/ex01';
 export default function App() {
   return (
     <View style={styles.container}>
---
+++ src/exemplos/ex01/index.js
@@ -0,0 +1,9 @@
+import {view, Text} from 'react native'; 
+export default function Exemplo001() {
+    return (
+        <View>
+            <Text> Exemplo 1</Text>
+            <Text> Introdução React Native com Expo</Text>
+            </View> 
+    ); 
+}
